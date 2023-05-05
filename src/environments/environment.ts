7b3a598e386fff9278325e796cc1c0ca23e00e71
Your Name <lebomariri505@gmail.com>
2023-05-05 21:51:16 +0200

    updated

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBn34Ou-NcwPpAZsuC0t7Q4vJUncC0acXg",
+        authDomain: "todos-c7163.firebaseapp.com",
+        projectId: "todos-c7163",
+        storageBucket: "todos-c7163.appspot.com",
+        messagingSenderId: "753371872818",
+        appId: "1:753371872818:web:655d55b69cbaba546d462b"
+      }
+};
+
